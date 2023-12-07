bf3c32595db0b69e388da5b9619381bc1fdb9548
UNIEN0 <unien7415@gmail.com>
2023-12-07 13:30:21 +0900

    【追加】玩家移动脚本

--- Assets/Scripts/Player/PlayerData_SO.cs
+++ Assets/Scripts/Player/PlayerData_SO.cs
@@ -7,14 +7,23 @@ public class PlayerData_SO : ScriptableObject
 {
     [Header("PlayerData")]
     [Header("基本资料")]
-    public string playerName;//名前
-    public Sprite playerIcon;//アイコン
-    public Sprite playerOnWorldSprite;//Game内の画像
-    [Multiline] public string playerDescription;//プレイヤーキャラクターの紹介
+    public string playerName;//名称
+    public Sprite playerIcon;//图标
+    public Sprite playerOnWorldSprite;//游戏内画像
+    [Multiline] public string playerDescription;//介绍
     [Space(10)]
     [Header("玩家属性")]
-    public int playerMaxHP;
+    public int playerMaxHP;//血量
     public int playerHP;
-    public float playerSpeed;
+    public float playerMaxSpeed;//移速
+    public float playerCurrentSpeed;
+    public float playerTurningSpeed;//转弯速度
+    public float playerAcceleration;//加速度
+
+    [Space(10)]
+    [Header("独立开关")]
+    public bool isDead;//是否死亡
+    public bool moveable;//是否可以移动
+    public bool controllable;//是否可以控制
 
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,70 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerData_SO playerData;
+
+    #region SO数据获取
+    //获取移速和转速
+    public float currentMoveSpeed
     {
-        
+        get { if (playerData != null) return playerData.playerCurrentSpeed; else return 0; }
+    }
+    public float playerTurningSpeed
+    {
+        get { if (playerData != null) return playerData.playerTurningSpeed; else return 0; }
+    }
+    public float playerAcceleration
+    {
+        get { if (playerData != null) return playerData.playerAcceleration; else return 0; }
+    }
+    public bool isDead
+    {
+        get { if (playerData != null) return playerData.isDead; else return true; }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    private Rigidbody2D rb2D;
+
+    private void Start()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        if (!isDead)
+        {
+            Move();
+            Turnigng();
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2D.velocity = transform.up * currentMoveSpeed * playerAcceleration;
+        }
+        else
+        {
+            rb2D.velocity = transform.up * currentMoveSpeed;
+        }
+    }
+
+    void Turnigng()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.right * -playerTurningSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.right * playerTurningSpeed * Time.deltaTime);
+        }
+    }
+
 }
