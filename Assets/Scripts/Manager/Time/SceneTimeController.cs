29da66cf69fb6935ea69a6e897305356dd166684
UNIEN <unien7415@gmail.com>
2023-12-17 12:42:47 +0900

    【追加】场景计时器

---
+++ Assets/Scripts/Manager/Time/SceneTimeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class SceneTimeController : MonoBehaviour
+{
+    private TMP_Text timeText;
+    private float sceneStartTime;
+
+    void Start()
+    {
+        timeText = gameObject.GetComponent<TMP_Text>();
+        //场景开始时时间归零
+        sceneStartTime = 0f;
+    }
+
+    void Update()
+    {
+        //增加时间
+        sceneStartTime += Time.deltaTime;
+        // 更新UI Text的文本内容，显示经过的时间
+        if (timeText != null)
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(sceneStartTime);
+            string formattedTime = string.Format("{0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+            timeText.text = formattedTime;
+        }
+    }
+}
