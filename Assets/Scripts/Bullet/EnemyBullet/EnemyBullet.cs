cbd848b937b913aa8d09c462a190b764ecfa6613
UNIEN <unien7415@gmail.com>
2023-12-20 22:51:26 +0900

    【未完】敌人子弹制作。目前问题：对象池回收的代码

--- Assets/Scripts/Bullet/EnemyBullet/EnemyBullet.cs
+++ Assets/Scripts/Bullet/EnemyBullet/EnemyBullet.cs
@@ -1,18 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float bulletExistenceTime;
+    [SerializeField] [ReadOnly]
+    private float newTime;
+    public int damage;
+
+    private EnemyBulletPool parentPool;
+
+    private void Update()
     {
-        
+        newTime += Time.deltaTime;
+        if (newTime >= bulletExistenceTime)
+        {
+            newTime = 0f;
+            parentPool.ReleaseExplosion(this);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //伤害传输
+            PlayerState player = gameObject.GetComponent<PlayerState>();
+            player.TakeDamage(damage);
+            //回收
+            parentPool.ReleaseExplosion(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/Enemy/Enemy/EnemyAttack/EnemyAttackLinearBullet.cs
@@ -0,0 +1,51 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 直线子弹
+/// </summary>
+public class EnemyAttackLinearBullet : MonoBehaviour
+{
+    Transform player;          // 玩家对象的Transform组件
+    public GameObject bulletPrefab;   // 子弹预制体
+    public Transform bulletSpawnPoint; // 子弹生成点
+    public float bulletForce = 10f;    // 子弹的推力
+    public float shootInterval = 2f;   // 射击间隔时间
+    public float detectionDistance = 5f; // 检测玩家距离
+
+    private EnemyBulletPool bulletPool;
+
+    void Start()
+    {
+        // 获取玩家对象的Transform组件
+        player = FindObjectOfType<PlayerState>()?.transform;
+        bulletPool = FindObjectOfType<EnemyBulletPool>();
+        // 启动协程，定期发射子弹
+        StartCoroutine(ShootAtPlayer());
+    }
+
+    IEnumerator ShootAtPlayer()
+    {
+        while (true)
+        {
+            // 计算敌人到玩家的距离
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+            // 如果距离小于检测距离，发射子弹
+            if (distanceToPlayer < detectionDistance)
+            {
+                // 创建子弹
+                //GameObject bullet = bulletPool.GetExplosion(transform.position, transform.rotation);
+
+                // 计算朝向玩家的方向
+                Vector2 direction = (player.position - transform.position).normalized;
+
+                // 使用AddForce方法为子弹添加推力
+                //bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletForce, ForceMode2D.Impulse);
+            }
+
+            // 在间隔时间后再次检测距离并发射子弹
+            yield return new WaitForSeconds(shootInterval);
+        }
+    }
+}
