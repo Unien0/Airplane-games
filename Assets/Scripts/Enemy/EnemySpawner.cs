66dff6f9537ef5a8bf6150e5c363d9ba752c92f4
UNIEN <unien7415@gmail.com>
2023-12-23 16:47:25 +0900

    【追加】敌人生成器，完成（需要追加对象池生成

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -13,9 +13,9 @@ public class EnemySpawner : MonoBehaviour
         //public List<int> enemyCount;
         [Header("敌人类型")]
         public List<EnemyGroup> enemyGroups;
-        public int waveQuota;
-        public float spawnInterval;
-        public int spawnCount;
+        [Tooltip("当前波额定值，不能为0")] public int waveQuota;//当前波额定值
+        [Tooltip("生成间隔")] public float spawnInterval;//生成间隔
+        [Tooltip("第几波，默认为0")] public int spawnCount;//第几波
 
     }
 
@@ -23,8 +23,8 @@ public class EnemySpawner : MonoBehaviour
     public class EnemyGroup
     {
         public string enemyName;
-        public int enemyCount;
-        public int spawnCount;
+        [Tooltip("敌人数")] public int enemyCount;//敌人数
+        [Tooltip("生成数，默认为0")] public int spawnCount;//生成数
         public GameObject enemyPrefab;
 
     }
@@ -32,13 +32,13 @@ public class EnemySpawner : MonoBehaviour
     public List<Wave> waves;
 
     [Header("参数")]
-    public int currentWaveCount;
+    [Tooltip("当前波次")] public int currentWaveCount;//当前波次
     float spawnTimer;
-    public float waveInterval;
-    public int enemiesAlive;
-    public int maxEnemiesAllowed;
-    public bool maxEnemiesReached = false;
-    private bool isWaveActive = false;
+    [Tooltip("波次间隔")] public float waveInterval;
+    [Tooltip("敌人存活数")]public int enemiesAlive;//敌人存活数
+    [Tooltip("允许的最大敌人数")] public int maxEnemiesAllowed;//允许的最大敌人数
+    [Tooltip("是否达到最大敌人数")] public bool maxEnemiesReached = false;//是否达到最大敌人数
+    [Tooltip("是否启动出怪")] private bool isWaveActive = false;//是否启动出怪
 
     [Header("敌人生成点")]
     public List<Transform> relativeSpawnPoints;
@@ -48,6 +48,76 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<PlayerState>().transform;
-        //CalculateWaveQuota();
+        CalculateWaveQuota();
+    }
+
+    void Update()
+    {
+        if (currentWaveCount < waves.Count && waves[currentWaveCount].spawnCount == 0 && !isWaveActive)
+        {
+            StartCoroutine(BeginNextWave());
+        }
+
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            spawnTimer = 0f;
+            SpawnEnemies();
+        }
+    }
+
+    IEnumerator BeginNextWave()
+    {
+        isWaveActive = true;
+        yield return new WaitForSeconds(waveInterval);
+
+        if (currentWaveCount < waves.Count - 1)
+        {
+            isWaveActive = false;
+            currentWaveCount++;
+            CalculateWaveQuota();
+        }
+    }
+
+    /// <summary>
+    /// 计算敌人波次
+    /// </summary>
+    void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var enemyGroup in waves[currentWaveQuota].enemyGroups)
+        {
+            currentWaveQuota += enemyGroup.enemyCount;
+        }
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+
+    }
+    void SpawnEnemies()
+    {
+
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota && !maxEnemiesReached)
+        {
+            foreach (var enemyGroup in waves[currentWaveCount].enemyGroups)
+            {
+                if (enemyGroup.spawnCount < enemyGroup.enemyCount)//如果当前生成数量小于设定的敌人数量，则让其生成
+                {
+                    //生成敌人，之后可以把他们改成使用对象池
+                    Instantiate(enemyGroup.enemyPrefab, player.position + relativeSpawnPoints[Random.Range(0, relativeSpawnPoints.Count)].position, Quaternion.identity);
+                    //Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+                    //Instantiate(enemyGroup.enemyPrefab, spawnPosition, Quaternion.identity);
+                    enemyGroup.enemyCount++;//增加出怪计时，用于增加波次
+                    waves[currentWaveCount].spawnCount++;
+                    enemiesAlive++;//增加怪物存活计时器，用于刷新怪物
+
+                    if (enemiesAlive >= maxEnemiesAllowed)//如果怪物存活数大于设定量，则返回
+                    {
+                        maxEnemiesReached = true;
+                        return;
+                    }
+
+                }
+            }
+        }
     }
 }
